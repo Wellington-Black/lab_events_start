2f03c4b3b45f15dd2e50660758043305a2dd862c
Wellington-Black <well1482@gmail.com>
2021-05-06 16:34:28 +0100

    third commit

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
 const textInput = document.querySelector('#new-item-form');
-textInput.addEventListener('input',handleInput);
+textInput.addEventListener('submit',handleInput);
 
 
 });
 
 const handleInput = function (event) {  
   event.preventDefault();
-  const resultInput = document.querySelector('#reading-list');
-  const book = create(event.target);
-  resultInput.appendChild(book);
-  // resultInput.textContent = `Title:${event.target.value}`;
+  const list = document.querySelector('#reading-list');
+  const book = document.createElement('li');
+  list.appendChild(book);
+  book.textContent = `Title:${event.target.title.value}`;
 
 }
 
